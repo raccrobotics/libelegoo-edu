687894b26099c7d491c6238d0db02513a2731b1a
Kapilesh Pennichetty <50639146+technocapeman@users.noreply.github.com>
2023-03-13 11:50:10 -0500

    Update ultrasonic_sensor.ino

--- ultrasonic_sensor/ultrasonic_sensor.ino
+++ ultrasonic_sensor/ultrasonic_sensor.ino
@@ -9,10 +9,10 @@ The ultrasonic sensor works by sending signals and measuring the time it takes f
 as we know the speed of the signals and the time it took for the signal to travel a certain distance. All of these calculations are done in the back end, on one of the driver files, all we have to do
 is call the function and we get the distance (if you want to see and understand the function that calculates the distance let us know and we would be happy to show you).
 
-DeviceDriverSet_UTLRASONIC Class
+DeviceDriverSet_UTLRASONIC Class - We must instantiate this in order to access the ultrasonic sensor
 
-DeviceDriverSet_ULTRASONIC_Init() - Configuring the pin numbers for the Ultrasonic sensor, allows the motherboard on the robot to talk to the sensor
-DeviceDriverSet_ULTRASONIC_Get() - Used to get the data from Ultrasonic sensor, all the necessary calculations are done in this function, it will return the distance of an obstacle that is in front of the Utlrasonic Sensor
+DeviceDriverSet_ULTRASONIC_Init() - Configures the pin numbers for the Ultrasonic sensor, allows the motherboard on the robot to talk to the sensor (call this in setup)
+DeviceDriverSet_ULTRASONIC_Get() - Retrieves the data from Ultrasonic sensor, returns the distance of an obstacle that is in front of the Ultrasonic Sensor (in centimeters)
 
 */
 
@@ -20,7 +20,7 @@ DeviceDriverSet_ULTRASONIC myUltrasonic; // Instantiates an ultrasonic object, u
 
 void setup()
 {
-  Serial.begin(9600); // Establish a data transfer rate
+  Serial.begin(9600); // Establish a data transfer rate of 9600 bits/second
   myUltrasonic.DeviceDriverSet_ULTRASONIC_Init(); // Configuring the pin numbers for the Ultrasonic sensor, allows the motherboard on the robot to talk to the sensor
 }
 
@@ -28,9 +28,9 @@ void loop()
 {
   unsigned int x = myUltrasonic.DeviceDriverSet_ULTRASONIC_Get(); // Calling the Get function to get data from the Ultrasonic sensor, it returns and Unsigned int that we store inside variable x (An Unsigned int is just an integer that is positive, includes 0)
 
-  /* Remeber x only contains the distance of an obstacle from the robot, we would then need to use motion control functions to move the robot around the obstacle */
+  /* Remember x only contains the distance of an obstacle from the robot, we would then need to use motion control functions to move the robot around the obstacle */
 
-  Serial.print("ULTRASONIC="); // Printing the distance (x) to the serial monitor - to access the serial monitor, select the magnifying glass icon on the top right of the IDE
+  Serial.print("ULTRASONIC="); // Printing the distance (x) to the serial monitor - to access the serial monitor, select the magnifying glass icon on the top right of the Arduino IDE
   Serial.print(x);
   Serial.println("cm");
 }
